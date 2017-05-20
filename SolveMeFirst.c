04c4c082b6def2221f09e49fd1e6ecb238e675e3
Akhil S <akhil.sudh@gmail.com>
2017-05-20 20:14:00 +0530

    Solve Me First

---
+++ SolveMeFirst.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int solveMeFirst(int a , int b)
+{
+	return (a + b)
+}
+
+int main()
+{
+	int x, y;
+	scanf("%d %d", &x, &y);
+	printf("%d", solveMeFirst(x, y));
+	return 0;
+}
